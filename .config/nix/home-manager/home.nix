e17f913901d94db3179bafcddb496f6dfdc6f635
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-08-21 17:46:33 +0000

    Nix config

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -7,31 +7,8 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
@@ -39,34 +16,51 @@
   };
 
   home = {
-    username = "e21146146";
-    homeDirectory = "/home/e21146146";
+    username = "1146146";
+    homeDirectory = "/home/1146146";
     shell = {
       enableNushellIntegration = true;
     };
+    packages = with pkgs; [
+      unzip
+      nodejs_24
+      go
+      python3Full
+      rustc
+      cargo
+    ];
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
+  programs.gh.enable = true;
   programs.nushell = {
     enable = true;
     environmentVariables = {
       http_proxy = "http://REDACTED:80/";
       https_proxy = "http://REDACTED:80/";
-      no_proxy = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
       HTTP_PROXY = "http://REDACTED:80/";
       HTTPS_PROXY = "http://REDACTED:80/";
+      no_proxy = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
       NO_PROXY = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
     };
+    settings = {
+      show_banner = false;
+    };
     shellAliases = {
-      ll = "ls -l";
+      ll = "ls -al";
+      nrs = "nixos-rebuild switch --flake ~/.config/nix#EC1414438";
+      hms = "home-manager switch --flake ~/.config/nix#1146146@EC1414438";
     };
   };
+  programs.starship.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+  programs.bun.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
---
+++ .config/nix/nixos/configuration.nix
@@ -0,0 +1,51 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  wsl.enable = true;
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  networking.hostName = "EC1414438";
+  networking.proxy.default = "http://REDACTED:80/";
+  networking.proxy.noProxy = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
+  security.pki.certificateFiles = [ 
+    "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    ./rtx-ca-bundle.crt
+  ];
+
+  users.users = {
+    "1146146" = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      shell = pkgs.nushell;
+    };
+  };
+  wsl.defaultUser = "1146146";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "25.05";
+}
