a2cb17ffcf52adcc0bf1c2ffff28b0fb9477c57e
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-08-21 18:09:59 +0000

    add all proxy

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -43,6 +43,8 @@
       https_proxy = "http://REDACTED:80/";
       HTTP_PROXY = "http://REDACTED:80/";
       HTTPS_PROXY = "http://REDACTED:80/";
+      all_proxy = "http://REDACTED:80/";
+      ALL_PROXY = "http://REDACTED:80/";
       no_proxy = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
       NO_PROXY = "localhost,127.0.0.1,.raytheon.com,.ray.com,.rtx.com,.utc.com,.adxrt.com,.registry.npmjs.org,.eks.amazonaws.com";
     };
