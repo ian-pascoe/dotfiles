129054df6205e7527977d5c73c1142632b44fd63
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-09-04 19:54:43 +0000

    several updates to theming, etc

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,17 +1,62 @@
 return {
-  { -- disable builtin explorer
+  {
     "folke/snacks.nvim",
+    ---@module "snacks"
+    ---@type snacks.Config
     opts = {
-      explorer = { enabled = false },
+      explorer = {
+        enabled = false, -- disable built-in file explorer
+      },
+      picker = {
+        sources = { -- show hidden files in pickers
+          files = { hidden = true },
+          grep = { hidden = true },
+          explorer = { hidden = true },
+        },
+      },
     },
   },
-  { -- use oil instead
+  { -- better file explorer
     "stevearc/oil.nvim",
     dependencies = { "echasnovski/mini.icons" },
     lazy = false,
-    opts = {},
+    ---@module "oil"
+    ---@type oil.setupOpts
+    opts = {
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+          -- always hide .git directory
+          if name:match("^%.git$") then
+            return true
+          end
+          return false
+        end,
+      },
+    },
     keys = {
-      { "-", "<cmd>Oil<cr>", desc = "File Explorer" },
+      {
+        "-",
+        function()
+          require("oil").open()
+        end,
+        desc = "File Explorer",
+      },
+      {
+        "<leader>e",
+        function()
+          require("oil").open(LazyVim.root())
+        end,
+        desc = "Explorer oil (root dir)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("oil").open(vim.fn.getcwd())
+        end,
+        desc = "Explorer oil (cwd)",
+      },
     },
   },
   { -- neogit
@@ -23,14 +68,17 @@ return {
       "sindrets/diffview.nvim", -- diff integration
       "folke/snacks.nvim", -- picker integration
     },
+    opts = {},
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
     },
   },
-  {
+  { -- show context at top of buffer
     "nvim-treesitter/nvim-treesitter-context",
     event = "LazyFile",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ---@type TSContext.Config
+    ---@diagnostic disable-next-line: missing-fields
     opts = {},
   },
 }
