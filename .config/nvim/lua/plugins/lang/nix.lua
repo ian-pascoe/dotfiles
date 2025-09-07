2dadbd9cbd11eb4096f41868686fa24a5004c22a
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-09-07 08:35:31 -0400

    nix updates, darwin rice

--- .config/nvim/lua/plugins/lang/nix.lua
+++ .config/nvim/lua/plugins/lang/nix.lua
@@ -5,6 +5,19 @@ return {
       ensure_installed = { "alejandra" },
     },
   },
+  { -- Use nixd instead of nil_ls for nix files
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = { enabled = false, mason = false },
+        nixd = {
+          mason = false,
+          cmd = { "nixd" },
+          filetypes = { "nix" },
+        },
+      },
+    },
+  },
   {
     "stevearc/conform.nvim",
     opts = {
