4457ab648877dd82de3c6efeddd5a61c9b690b41
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-09-08 15:47:50 -0400

    sketchybar

--- .config/nix/modules/darwin/home/programs/graphical/sketchybar/config/init.lua
+++ .config/nix/modules/darwin/home/programs/graphical/sketchybar/config/init.lua
@@ -1,3 +1,10 @@
+Sbar = require("sketchybar")
+
+Sbar.begin_config()
 require("bar")
 require("default")
 require("items")
+Sbar.hotload(true)
+Sbar.end_config()
+
+Sbar.event_loop()
--- .config/nix/modules/darwin/home/programs/graphical/sketchybar/config/settings.lua
+++ .config/nix/modules/darwin/home/programs/graphical/sketchybar/config/settings.lua
@@ -1,4 +1,18 @@
 return {
-	font = "SF Pro",
 	paddings = 3,
+	group_paddings = 5,
+
+	-- Alternatively, this is a font config for JetBrainsMono Nerd Font
+	font = {
+		text = "JetBrainsMono", -- Used for text
+		numbers = "JetBrainsMono", -- Used for numbers
+		space_numbers = "JetBrainsMono", -- Used for space numbers
+		style_map = {
+			["Regular"] = "Regular",
+			["Semibold"] = "Medium",
+			["Bold"] = "SemiBold",
+			["Heavy"] = "Bold",
+			["Black"] = "ExtraBold",
+		},
+	},
 }
