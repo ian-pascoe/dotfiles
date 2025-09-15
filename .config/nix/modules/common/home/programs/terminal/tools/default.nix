b1edd9d8c5ce3a086a4a918ecb8cd3d0976ed2f3
Ian Pascoe <ian.pascoe@rtx.com>
2025-09-15 16:20:42 +0000

    Add yazi

--- .config/nix/modules/common/home/programs/terminal/tools/default.nix
+++ .config/nix/modules/common/home/programs/terminal/tools/default.nix
@@ -17,6 +17,7 @@
     ./ripgrep
     ./starship
     ./uv
+    ./yazi
     ./zoxide
   ];
 }
---
+++ .config/nix/modules/common/home/programs/terminal/tools/yazi/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  dotfiles,
+  ...
+}: {
+  import = [
+    ../../../../../../util/home/dotfiles
+  ];
+
+  programs.yazi = {
+    enable = true;
+  };
+
+  xdg.configFile.yazi = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/.config/yazi";
+    force = true;
+  };
+}
