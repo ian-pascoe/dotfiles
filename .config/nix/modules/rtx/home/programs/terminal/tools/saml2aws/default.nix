67d000f302d3bb2a637482f4c58b8f45a5771e73
Ian Pascoe <ian.pascoe@rtx.com>
2025-09-17 13:13:30 +0000

    fix playwright

--- .config/nix/modules/rtx/home/programs/terminal/tools/saml2aws/default.nix
+++ .config/nix/modules/rtx/home/programs/terminal/tools/saml2aws/default.nix
@@ -1,12 +1,18 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    playwright
     playwright-driver
-    playwright.browsers
+    playwright-driver.browsers
     saml2aws
   ];
 
   home.file.".saml2aws" = {
     source = ./config/saml2aws.ini;
     force = true;
+  };
+
+  home.sessionVariables = {
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
   };
 }
