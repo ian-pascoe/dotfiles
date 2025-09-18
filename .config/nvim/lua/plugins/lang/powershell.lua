d56ad6ee843697f3d4d41ef8084bbec6c167053e
Ian Pascoe <ian.pascoe@rtx.com>
2025-09-18 15:00:16 +0000

    fix

--- .config/nvim/lua/plugins/lang/powershell.lua
+++ .config/nvim/lua/plugins/lang/powershell.lua
@@ -6,6 +6,7 @@ return {
         ---@type vim.lsp.ClientConfig
         ---@diagnostic disable-next-line: missing-fields
         powershell_es = {
+          enabled = vim.fn.executable("pwsh") == 1,
           capabilities = require("blink-cmp").get_lsp_capabilities(nil, true),
           init_options = {
             enableProfileLoading = false,
