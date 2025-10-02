dae201b05d045227f90c197e95393f7efc80c9e8
Ian Pascoe <ian.pascoe@rtx.com>
2025-10-02 20:27:44 +0000

    fix: proxy config

--- .config/nix/homes/e21146146@EC1414438/default.nix
+++ .config/nix/homes/e21146146@EC1414438/default.nix
@@ -11,7 +11,7 @@
     homeDirectory = "/home/e21146146";
 
     shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake ~/.config/nix#EC1414438";
+      nrs = "source ~/proxy.sh; sudo -E nixos-rebuild switch --flake ~/.config/nix#EC1414438 --impure";
       nfu = "nix flake update --flake ~/.config/nix";
       ncg = "sudo nix-collect-garbage -d";
     };
