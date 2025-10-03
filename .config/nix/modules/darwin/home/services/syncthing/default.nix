e5fb6105ee10a083d0a6ff7ee1ca58446e5db3f9
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-10-03 12:01:05 -0400

    feature: add alias to restart-syncthing

--- .config/nix/modules/darwin/home/services/syncthing/default.nix
+++ .config/nix/modules/darwin/home/services/syncthing/default.nix
@@ -2,4 +2,8 @@
   services.syncthing = {
     enable = true;
   };
+
+  home.shellAliases = {
+    restart-syncthing = ''launchctl kickstart -k gui/"$(id -u)"/org.nix-community.home.syncthing'';
+  };
 }
