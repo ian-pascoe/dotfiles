969cf82d561158777ee4d6004dfeddc85551d095
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-10-11 07:33:36 -0400

    add sidekick

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- use blink.cmp for ai completions
-vim.g.ai_cmp = true
-
 if LazyVim.is_win() then
   LazyVim.terminal.setup("powershell") -- I use nushell on Windows
 end
