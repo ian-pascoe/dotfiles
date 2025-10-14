39f08c99c186b81125512592f7767e58068aceaf
Ian Pascoe <ian.pascoe@rtx.com>
2025-10-14 15:17:32 +0000

    fix: tweaks

--- .config/nix/modules/common/home/programs/terminal/tools/opencode/default.nix
+++ .config/nix/modules/common/home/programs/terminal/tools/opencode/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   dotfiles,
   ...
@@ -7,9 +8,13 @@
   imports = [
     ../../../../../../util/home/dotfiles
   ];
+
   programs.opencode = {
     enable = true;
+    # TODO: Switch back to official package when it's fixed.
+    package = pkgs.nur.repos.falconprogrammer.opencode-sst;
   };
+
   xdg.configFile.opencode = {
     source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/.config/opencode";
     force = true;
--- .config/nix/modules/rtx/security/default.nix
+++ .config/nix/modules/rtx/security/default.nix
@@ -18,15 +18,18 @@
   environment = {
     sessionVariables = {
       # SSL certificate settings
-      SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+      SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
       SSL_CERT_DIR = "/etc/ssl/certs";
-      REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+      REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-bundle.crt";
 
       # Java specific
       JDK_JAVA_OPTIONS = "-Djavax.net.ssl.trustStore=${rtxCerts.trustStore}";
       JAVA_TOOL_OPTIONS = "-Djavax.net.ssl.trustStore=${rtxCerts.trustStore}";
       JAVA_OPTS = "-Djavax.net.ssl.trustStore=${rtxCerts.trustStore}";
       JAVA_OPTIONS = "-Djavax.net.ssl.trustStore=${rtxCerts.trustStore}";
+
+      # Node Specific
+      NODE_EXTRA_CA_CERTS = "/etc/ssl/certs/ca-bundle.crt";
     };
   };
 }
