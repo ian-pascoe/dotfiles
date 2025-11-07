ba89712a94d1eb504e7799c65bec7370dc0a6240
Ian Pascoe <ian.pascoe@rtx.com>
2025-11-07 12:52:39 +0000

    fix: tmux plugin config

--- nix/modules/common/home/programs/terminal/tools/tmux/default.nix
+++ nix/modules/common/home/programs/terminal/tools/tmux/default.nix
@@ -11,17 +11,18 @@
 
   programs.tmux = {
     enable = true;
-    plugins = with pkgs.tmuxPlugins; [
-      resurrect
-      continuum
-      tmux-fzf
-    ];
   };
 
   xdg.configFile = {
     "tmux/tmux.conf" = {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/tmux/tmux.conf";
       force = true;
+    };
+    "tmux/plugins/continuum" = {
+      source = pkgs.tmuxPlugins.continuum;
+    };
+    "tmux/plugins/resurrect" = {
+      source = pkgs.tmuxPlugins.resurrect;
     };
   };
 }
