208640291e1b213cc6e1ba85c290a7948fbbc550
Ian Pascoe <ian.pascoe@rtx.com>
2025-11-10 12:25:13 +0000

    fix: zellij

--- nix/modules/common/home/programs/terminal/tools/zellij/default.nix
+++ nix/modules/common/home/programs/terminal/tools/zellij/default.nix
@@ -1,15 +1,8 @@
 {
-  pkgs,
   config,
   dotfiles,
   ...
 }:
-let
-  zjstatus = pkgs.fetchurl {
-    url = "https://github.com/dj95/zjstatus/releases/download/v0.21.1/zjstatus.wasm";
-    hash = "sha256-0wpa6hvzizx8zjbz2cja9n4s4yd75vn20q8g5nr9fsr1kff0g9s5";
-  };
-in
 {
   imports = [
     ../../../../../../util/home/dotfiles
@@ -27,9 +20,6 @@ in
     "zellij/layouts" = {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/zellij/layouts";
       force = true;
-    };
-    "zellij/plugins/zjstatus.wasm" = {
-      source = zjstatus;
     };
   };
 }
