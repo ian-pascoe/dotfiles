6825bd7f114ff1f39f94c29d151f55900b2b9be1
Ian Pascoe <ian.pascoe@rtx.com>
2025-11-14 17:24:20 +0000

    fixes

--- nix/modules/common/networking/default.nix
+++ nix/modules/common/networking/default.nix
@@ -1,8 +1,21 @@
+{ config, ... }:
 {
   networking = {
     proxy = {
-      default = builtins.getEnv "all_proxy";
+      allProxy = builtins.getEnv "all_proxy";
+      httpProxy = builtins.getEnv "http_proxy";
+      httpsProxy = builtins.getEnv "https_proxy";
+      ftpProxy = builtins.getEnv "ftp_proxy";
       noProxy = builtins.getEnv "no_proxy";
     };
+  };
+
+  # Set upper case versions of the proxy environment variables
+  environment.variables = {
+    ALL_PROXY = config.networking.proxy.allProxy;
+    HTTP_PROXY = config.networking.proxy.httpProxy;
+    HTTPS_PROXY = config.networking.proxy.httpsProxy;
+    FTP_PROXY = config.networking.proxy.ftpProxy;
+    NO_PROXY = config.networking.proxy.noProxy;
   };
 }
