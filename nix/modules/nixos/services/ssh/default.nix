0e40625f33f38d87f0323599ec65b36347ffbafa
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-12-01 15:24:44 -0500

    chore: update

--- nix/modules/nixos/services/ssh/default.nix
+++ nix/modules/nixos/services/ssh/default.nix
@@ -3,7 +3,9 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = lib.mkDefault true;
+    allowSFTP = lib.mkDefault true;
     settings = {
+      PermitRootLogin = lib.mkDefault false;
       X11Forwarding = lib.mkDefault true;
     };
   };
