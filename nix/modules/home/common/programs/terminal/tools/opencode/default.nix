29fe2e89f1f0ce720b2c5c52d568f68978affc14
Ian Pascoe <ian.g.pascoe@gmail.com>
2026-01-08 17:24:39 -0500

    fix: opencode

--- nix/modules/home/common/programs/terminal/tools/opencode/default.nix
+++ nix/modules/home/common/programs/terminal/tools/opencode/default.nix
@@ -5,14 +5,6 @@
   dotfiles,
   ...
 }:
-let
-  superpowers = pkgs.fetchFromGitHub {
-    owner = "obra";
-    repo = "superpowers";
-    rev = "v4.0.3";
-    hash = "sha256-0/biMK5A9DwXI/UeouBX2aopkUslzJPiNi+eZFkkzXI=";
-  };
-in
 {
   programs.opencode = {
     enable = lib.mkDefault pkgs.stdenv.isLinux; # handled via homebrew on mac
@@ -26,43 +18,18 @@ in
   };
 
   xdg.configFile = {
-    "opencode/opencode.jsonc" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/opencode.jsonc";
-      force = true;
-    };
-    "opencode/oh-my-opencode.jsonc" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/oh-my-opencode.jsonc";
-      force = true;
-    };
-    "opencode/antigravity.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/antigravity.json";
-      force = true;
-    };
-    "opencode/agent" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/agent";
-      force = true;
-    };
-    "opencode/command" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/command";
-      force = true;
-    };
-    "opencode/plugin" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/plugin";
-      force = true;
-    };
-    "opencode/skill" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode/skill";
-      force = true;
-    };
-    "opencode/superpowers" = {
-      source = superpowers;
+    opencode = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles.path}/config/opencode";
       force = true;
     };
   };
 
   home.activation = {
     setupSuperpowers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ln -sf ${config.xdg.configHome}/opencode/superpowers/.opencode/plugin/superpowers.js ${config.xdg.configHome}/opencode/plugin/superpowers.js
+      if [ ! -d ${config.xdg.configHome}/opencode/superpowers ]; then
+        ${pkgs.git}/bin/git clone https://github.com/obra/superpowers.git ${config.xdg.configHome}/opencode/superpowers
+      fi
+      /bin/ln -snf ${config.xdg.configHome}/opencode/superpowers/.opencode/plugin/superpowers.js ${config.xdg.configHome}/opencode/plugin/superpowers.js
     '';
   };
 }
