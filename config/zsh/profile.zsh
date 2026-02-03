3770bb1ae5c1a8bd9273ee57b1efa7a75ffa9597
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2026-02-03 05:55:06 -0500

    updates

--- config/zsh/profile.zsh
+++ config/zsh/profile.zsh
@@ -3,4 +3,8 @@
 ##===========================================
 
 ## Homebrew
-eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+fi
