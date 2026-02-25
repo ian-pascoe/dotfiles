7342534fbe58ff91a518f065739e14087a733c4d
Ian Pascoe <ian.g.pascoe@gmail.com>
2026-02-25 09:11:02 -0500

    updates

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -9,7 +9,7 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 ## OpenCode
-export OPENCODE_EXPERIMENTAL=true
+export OPENCODE_EXPERIMENTAL="1"
 
 ## Editor
 if command -v nvim &>/dev/null; then
